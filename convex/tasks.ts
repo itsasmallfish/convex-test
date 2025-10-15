d4f2b0854cfdb4c0388bce8052d4e32f0c5dba99
violistic <vihennig0@gmail.com>
2025-10-15 16:10:19 +0200

    init standart fullstack todo management functionality

---
+++ convex/tasks.ts
@@ -0,0 +1,35 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const getTasks = query({
+    args: {},
+    handler: async (ctx) => {
+        return await ctx.db.query("tasks").collect();
+    }
+});
+
+export const addTask = mutation({
+    args: { text: v.string() },
+    handler: async (ctx, args) => {
+        return await ctx.db.insert("tasks", args);
+    }
+});
+
+export const removeTask = mutation({
+    args: { id: v.id("tasks") },
+    handler: async (ctx, args) => {
+        return await ctx.db.delete(args.id);
+    }
+});
+
+export const toggleTask = mutation({
+    args: { id: v.id("tasks") },
+    handler: async (ctx, args) => {
+        const task = await ctx.db.get(args.id);
+        if (!task) {
+            throw new Error("Task not found");
+        }
+
+        return await ctx.db.patch(args.id, { completed: !task.completed });
+    }
+});
